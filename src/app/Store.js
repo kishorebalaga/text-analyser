5fbaaba3de62b3076eb11346472347786107574e
Kishore-Balaga <kishore.balaga@ibm.com>
2024-02-03 10:40:49 +0800

    Remove counter

--- src/app/Store.js
+++ src/app/Store.js
@@ -1,7 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/CounterSlice";
 import wordsReducer from "../features/display/top/WordsSlice";
 
 export const store = configureStore({
-  reducer: { counter: counterReducer, words: wordsReducer },
+  reducer: { words: wordsReducer },
 });
